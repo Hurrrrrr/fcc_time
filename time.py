cf308f42b9925d2c23b07862b86ecc2752b2e604
Hurrrrrr <jonahpalmer@gmail.com>
2023-03-31 13:56:09 -0400

    continue minutes_to_time()

--- time.py
+++ time.py
@@ -1,4 +1,6 @@
 MINUTES_TO_HOURS = 60
+MINUTES_IN_DAY = 1440
+today = 0
 
 def add_time(start_time, duration, day=None):
 
@@ -19,7 +21,8 @@ def parse_input(start_time, duration, day=None):
     start_time_parsed = []
     duration_parsed = []
     if day != None:
-        day_lower = day.lower()
+        day_parsed = day.lower()
+        set_day(day_parsed)
 
     start_time_replaced = start_time.replace(" ", ":")
 
@@ -37,10 +40,30 @@ def parse_input(start_time, duration, day=None):
             duration_parsed.append(int(item))
         else:
             duration_parsed.append(item)
+    
 
-    # format: [H, M, "AM/PM"], [H, M]
+
+    # format: [H, M, "AM/PM"], [H, M], ["day"]
     return [start_time_parsed, duration_parsed]
 
+
+
+def set_day(parsed_day):
+
+    if day_parsed == "monday":
+        today = 1
+    elif day_parsed == "tuesday":
+        today = 2
+    elif day_parsed == "wednesday":
+        today = 3
+    elif day_parsed == "thursday":
+        today = 4
+    elif day_parsed == "friday":
+        today = 5
+    elif day_parsed == "saturday":
+        today = 6
+    elif day_parsed == "sunday":
+        today = 7
 
 
 def time_to_minutes(parsed_start_time):
@@ -71,17 +94,50 @@ def minutes_to_time(start_in_minutes, duration_in_minutes):
 
     minutes = start_in_minutes + duration_in_minutes
     hours = 0
+    days = 0
+    time_AM = None
+    time_days = 0
+    time_hours = 0
+    time_minutes = 0
+    clock_time = []
 
-    while minutes > 59:
+    while minutes >= MINUTES_IN_DAY:
+        days = days + 1
+        minutes = minutes - MINUTES_IN_DAY
+
+    while minutes >= 60:
         hours = hours + 1
         minutes = minutes - 60
+    
+    if (hours % 24) <= 12:
+        time_AM = True
+    else:
+        time_AM = False
+        hours = hours - 12
+    #######
+
+    # result is multiple days later
+    if start_in_minutes + duration_in_minutes >= (MINUTES_IN_DAY * 2):
+        return
+
+    #result is the next day
+    if start_in_minutes + duration_in_minutes >= MINUTES_IN_DAY:
+        return
+
+    # result is same day
+    if start_in_minutes + duration_in_minutes < MINUTES_IN_DAY:
+        return
+
+
+    
+
     
     # better to refactor to add days now
     
 
 
 
-test_input = ("9:50 PM", "3:10")
+test_input = ("9:50 PM", "0:10", "tuesday")
 
 parsed_test = parse_input(test_input[0], test_input[1])
 
